b489fcddd7942c4b3bae213e42d3ad87c956a90d
Jana <janaemam5768@gmail.com>
2026-03-25 05:47:32 +0200

    implement Marks Management system using dynamic arrays
    - Created a class to manage student marks using dynamic memory allocation.
    - Implemented a constructor to initialize the marks array based on student count.
    - Added countPass() function to filter students with marks >= 50.
    - Implemented maxMark() to find the highest score in the array.
    - Used a Destructor to safely deallocate memory and prevent leaks.

--- C++ class.cpp
+++ C++ class.cpp
@@ -1,49 +1,87 @@
 #include <iostream>
 using namespace std;
+#include <string>
 
-private:
+/*Stores n student marks in a dynamic array.
+Has a function input() to read the marks from the user.
+Has a function countPass() that counts how many students passed
+(mark ≥ 50).
+Has a function maxMark() to find the highest mark.
+Use a constructor to create the dynamic array.
+Use a destructor to delete it.
+In main():
+Read number of students n
+Create object
+Input marks
+Print:
+number of students who passed
+highest mark*/
+class Markes {
+private :
 	int* arr;
 	int size;
+	 
+
 public:
-	Numbers(int n) {
-		size = n;
+	Markes(int m) {
+		size = m;
 		arr = new int[size];
 	}
+	 
+	 
 	void input() {
 		for (int i = 0; i < size; i++) {
-		
 			cin >> arr[i];
 		}
 	}
-	int maxvalue() {
+	//Has a function countPass() that counts how many students passed
+	//(mark ≥ 50).
+	int countpass() {
+		int count = 0;
+		for (int i = 0; i < size; i++) {
+			if (arr[i] >= 50) {
+				count++;
+			}
+		
+		}
+		
+		return count;
+	}
+	//Has a function maxMark() to find the highest mark.
+	int maxmark() {
 		int max = arr[0];
 		for (int i = 1; i < size; i++) {
 			if (arr[i] > max) {
 				max = arr[i];
-
 			}
 		}
 		return max;
-	}
-	int sum(){
-		int temp=0;
-		for (int i = 0; i < size; i++) {
-			temp += arr[i];
-			
-		}
-		return temp;
-	}
-	~Numbers() {
+	 }
+	~Markes() {
 		delete[] arr;
 	}
-};
-int main() {
-	int n;
-	cin >> n; 
-	Numbers num(n);
-	num.input();
-	cout <<"max number is:" << num.maxvalue() << endl;
-	cout << "the sum of the numbers are:" << num.sum() << endl;
 
+	/*Use a constructor to create the dynamic array.
+Use a destructor to delete it.*/
+
+
+};
+
+
+
+int main() {
+	/*
+Read number of students n
+Create object
+Input marks
+Print:
+number of students who passed
+highest mark*/
+	int m;
+	cin >> m;
+	Markes mark (m);
+	mark.input();
+	cout << "Number of students who pass: " << mark.countpass() << endl;
+	cout << "Max mark: " << mark.maxmark() << endl;
 	return 0;
 }
